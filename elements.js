fc6dc72169ebcb37b820c8f5953538b1a35ec8a7
thewizardwilliam <95314818+thewizardwilliam@users.noreply.github.com>
2022-03-13 09:45:29 -0400

    Update elements.js

--- elements.js
+++ elements.js
@@ -1,5 +1,1 @@
 window.location.replace(document.getElementsByTagName("redirect")[0].innerHTML);
-for (let i = 0;document.getElementsByTagName("json")[i] != undefined;i++) {
-  JSON.parse(document.getElementsByTagName("json").innerHTML);
-  document.getElementsByTagName("json").hidden = "true";
-}
