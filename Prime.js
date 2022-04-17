daafb3b89c7d6570ddb4910955f782ab5d09cafa
thewizardwilliam <95314818+thewizardwilliam@users.noreply.github.com>
2022-04-17 11:04:50 -0400

    Update Prime.js

--- Prime.js
+++ Prime.js
@@ -3,9 +3,9 @@ function prime(num) {
   else if (num === 2) return true;
   else if (Number.isInteger(num) === false) {
     if (num * 1 === NaN) return NaN;
-    else return new Error("Syntax Error: Expected an integer, but a decimal value was provided.",{name:"Synax Error",message:"Expected an integer, but a decimal value was provided."})
+    else return "Syntax Error: Expected an integer, but a decimal value was provided."
   }
-  else if (num < 1) return new Error("Range Error: Number is too low.",{name:"Range Error",message:"Number is too low."});
+  else if (num < 1) return "Range Error: Number is too low."
   else {
     let r = true;
     for (let i = 2;i < num - 1;i++) {
